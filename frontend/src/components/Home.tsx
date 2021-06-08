b1cac948f95f4134fa4d52cfe019ba1a70e534a1
dylan <dylan@dylans-MacBook-Pro.local>
2021-06-08 12:23:33 -0600

    Style metamask button

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { LinearProgress } from '@material-ui/core/';
-import Bracket from './Bracket';
+import  {Button} from "antd"
+ import Bracket from './Bracket';
 import LatestActivityCarousel from './LatestActivityCarousel';
 import {gameArray, dummyArray } from '../fixtures/gameData';
 
 import gitcoinDance from "../assets/gitcoin/gitcoinDance.svg"
 import nftDanceOff from "../assets/gitcoin/nftDanceOff.svg"
+import iconWallet from "../assets/gitcoin/iconWallet.svg"
 
 export function Home(props : any) {
   const [gameData1, setGameData1] = useState<any>(null);
@@ -29,12 +31,15 @@ export function Home(props : any) {
       <div style={{display: "flex"}}>
       <img style={{margin: "auto"}} src={gitcoinDance} alt="Gitcoin Dance Logo" />
       </div>
-      <div style={{display: "flex", justifyContent: "flex-end"}}>
+      <div style={{float: "right", margin: "-50px 50px"}}>
       <Link to="/about">
-       <h1 style={{display: "inline"}} className="yellowText marginTen">ABOUT</h1>
+       <h4 style={{display: "inline"}} className="yellowText marginTen">ABOUT</h4>
       </Link>
       <Link to="/connect">
-       <h1 style={{display: "inline"}} className="yellowText marginTen">CONNECT WALLET</h1>
+       <Button style={{padding: "5px", borderRadius: "4px"}} type="ghost" className="marginTen meta-mask-button">        
+          <img style={{verticalAlign: "baseline", margin: "0px 10px 0px 0px"}} src={iconWallet} alt="wallet" />
+          <span className="gradeText">METAMASK</span>
+      </Button>
       </Link>
       </div>
       <header className="bg-grid" style={{height: "120px"}}>
